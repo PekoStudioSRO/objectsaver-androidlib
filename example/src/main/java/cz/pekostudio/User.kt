0f3754e325c53c9525b642c3b9c194e9d7c96138
Miroslav Hýbler <miroslav@peko-studio.cz>
2020-11-27 11:14:39 +0100

    1.+.2 -> navýšení targetSdk na 30, navýšení kotlinu na 1.4.20, navýšení gradle na 4.1.1, odebríní plugina kotlin-android-extensions

--- example/src/main/java/cz/pekostudio/User.kt
+++ example/src/main/java/cz/pekostudio/User.kt
@@ -3,6 +3,6 @@ package cz.pekostudio
 /**
  * Created by Lukas Urbanek on 04/06/2020.
  */
-data class User(var test: Int) : SavedData() {
+data class User(var test: Int) {
 
 }
---
+++ example/src/main/java/cz/pekostudio/navexample/MainActivity.kt
@@ -0,0 +1,14 @@
+package cz.pekostudio.navexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cz.pekostudio.test
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        test()
+    }
+}
